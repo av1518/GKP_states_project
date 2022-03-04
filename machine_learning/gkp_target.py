05f672b5494ec23aca27360d300d9e316af43cc3
Gefaell <79812190+Gefaell@users.noreply.github.com>
2022-03-04 10:31:12 +0000

    Comparison between sf GKP and manual GKP

--- machine_learning/gkp_target.py
+++ machine_learning/gkp_target.py
@@ -5,13 +5,18 @@ import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import numpy.polynomial.hermite as Herm
 import math
+import strawberryfields as sf
+from strawberryfields.ops import *
+from qutip import wigner, Qobj, wigner_cmap
+import matplotlib as mpl
+from matplotlib import cm
 
 m=1.
 w=1.
 hbar=1.
 
-dB = 24
-cutoff = 15
+dB = 12 # the aim is 24.4
+cutoff = 30
 
 eps = np.finfo(float).eps
 
@@ -49,17 +54,6 @@ def model_gkp(delta_dB):
 
     return q, gkp
 
-x = model_gkp(dB)[0]
-gk = model_gkp(dB)[1]
-
-plt.figure()
-plt.plot(x,gk, label = f'{dB}dB')
-plt.xlabel('q')
-plt.ylabel('psi(q)')
-plt.legend()
-
-# %%
-
 def hermite(x, n):
     xi = np.sqrt(m*w/hbar)*x
     herm_coeffs = np.zeros(n+1)
@@ -83,8 +77,26 @@ def fock_coeffs(x,gkp_wf,cutoff):
             cs[i]=0.0
     return np.array(cs)
 
+def adj(v):
+    adjo = []
+    for i in range(len(v)):
+        adjo.append([v[i]])
+    return np.array(adjo)
+
+#%%
+
+x = model_gkp(dB)[0]
+gk = model_gkp(dB)[1]
 cns = fock_coeffs(x,gk,cutoff)
 fock_range = np.arange(0,cutoff,1)
+ad_cns = adj(cns)
+dm_target = ad_cns*cns
+
+plt.figure()
+plt.plot(x,gk**2, label = f'{dB}dB')
+plt.xlabel('x')
+plt.ylabel('Probability')
+plt.legend()
 
 # plt.figure()
 # plt.bar(fock_range,cns)
@@ -92,17 +104,51 @@ fock_range = np.arange(0,cutoff,1)
 # plt.ylabel('Fock amplitudes')
 
 plt.figure()
-plt.bar(fock_range,(np.absolute(cns))**2)
+plt.bar(fock_range,(cns)**2)
 plt.xlabel('Number basis')
 plt.ylabel('Fock probabilities')
 
-def adj(v):
-    adjo = []
-    for i in range(len(v)):
-        adjo.append([v[i]])
-    return np.array(adjo)
+# xvec = np.linspace(-6,6, 800)
+# Wp = wigner(Qobj(dm_target), xvec, xvec)
+# wmap = wigner_cmap(Wp)
+# sc1 = np.max(Wp)
+# nrm = mpl.colors.Normalize(-sc1, sc1)
+# fig, axes = plt.subplots(1, 1, figsize=(5, 4))
+# plt1 = axes.contourf(xvec, xvec, Wp, 60,  cmap=cm.RdBu, norm=nrm)
+# axes.contour(xvec, xvec, Wp, 60,  cmap=cm.RdBu, norm=nrm)
+# axes.set_title("Wigner function of our target state")
+# cb1 = fig.colorbar(plt1, ax=axes)
+# fig.tight_layout()
 
-ad_cns = adj(cns)
-dm_target = ad_cns*cns
+#%%
 
-#plt.show()
+prog_gkp = sf.Program(1)
+e = 10**(-dB/10)
+with prog_gkp.context as q:
+    GKP(epsilon = e) | q[0]
+    
+eng_gkp = sf.Engine('fock', backend_options = {'cutoff_dim': cutoff}) #
+gkp = eng_gkp.run(prog_gkp).state
+
+target_state = gkp
+target_dm = gkp.dm()
+
+xvec = np.linspace(-6,6, 800)
+Wp2 = wigner(Qobj(target_dm), xvec, xvec)
+wmap2 = wigner_cmap(Wp2)
+sc12 = np.max(Wp2)
+nrm2 = mpl.colors.Normalize(-sc12, sc12)
+fig2, axes2 = plt.subplots(1, 1, figsize=(5, 4))
+plt2 = axes2.contourf(xvec, xvec, Wp2, 60,  cmap=cm.RdBu, norm=nrm2)
+axes2.contour(xvec, xvec, Wp2, 60,  cmap=cm.RdBu, norm=nrm2)
+axes2.set_title("Wigner function of SF target state")
+cb12 = fig2.colorbar(plt2, ax=axes2)
+fig2.tight_layout()
+
+# plt.figure()
+# plt.title('Wigner cut of target state')
+# plt.plot(xvec, Wp2[800//2,:])
+# plt.ylabel(r"W(q,0)")
+# plt.xlabel(r"q")
+
+plt.show()
