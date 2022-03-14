ed7b6b2fa86f54e2d5cff479518c5512b649bd28
Gefaell <79812190+Gefaell@users.noreply.github.com>
2022-03-14 14:07:21 +0000

    Update trace_dist.py

--- GKPs_from_cats/trace_dist.py
+++ GKPs_from_cats/trace_dist.py
@@ -9,8 +9,8 @@ from gkp_target import target_dm, cutoff, dB
 import copy
 from tqdm import tqdm
 
-a_vals = np.arange(0.,4.0,0.05)
-s_vals = np.arange(0.,4.0,0.05)
+a_vals = np.arange(0,4.5,0.05)
+s_vals = np.arange(0,4.5,0.05)
 
 s, a = np.meshgrid(a_vals, s_vals)
 #%%
@@ -49,26 +49,47 @@ for i in tqdm(range(len(a_vals))):
         eng.reset()
 
 trs = np.array(tr)
+np.save('trac_dist_explore', trs)
+
+#%%
+trac_dists = np.load('trac_dist_explore.npy',allow_pickle=True)
+
+lists = []
+for i in range(len(trac_dists)):
+    lists.append(trac_dists[i])
+
+trace_dista = np.vstack((lists))
 
 fig1, ax1 = plt.subplots(subplot_kw={"projection": "3d"})
 ax1.set_xlabel(r'$a$')
 ax1.set_ylabel(r'$s$')
 ax1.set_zlabel(r"$Tr$")
-threeD_plot = ax1.plot_surface(a, s, trs, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+threeD_plot = ax1.plot_surface(a, s, trace_dista, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 fig1.colorbar(threeD_plot, shrink=0.5)
 plt.tight_layout()
 
+ag = np.sqrt(np.pi)*np.exp(s_vals)
+
+sgs = []
+ags = []
+for i in range(len(ag)):
+    if ag[i] <= np.max(a_vals):
+        ags.append(ag[i])
+        sgs.append(s_vals[i])
+
 grid = 800
 xvec = np.linspace(-6,6, grid)
-sc1 = np.max(trs)
-nrm = mpl.colors.Normalize(np.min(trs), sc1)
+sc1 = np.max(trace_dista)
+nrm = mpl.colors.Normalize(np.min(trace_dista), sc1)
 fig, axes = plt.subplots(1, 1, figsize=(5, 4))
-plt1 = axes.contourf(a, s, trs, 60,  cmap=cm.RdBu, norm=nrm)
-axes.contour(a, s, trs, 60,  cmap=cm.RdBu, norm=nrm)
-axes.set_title(f'Trace distance for {dB} dB')
+plt1 = axes.contourf(a, s, trace_dista, 60,  cmap=cm.coolwarm, norm=nrm)
+axes.contour(a, s, trace_dista, 60,  cmap=cm.coolwarm, norm=nrm)
+axes.plot(ags, sgs, color = 'black')
+axes.set_title(f'Parameter space for {dB} dB target')
 axes.set_xlabel('a')
 axes.set_ylabel('s')
 cb1 = fig.colorbar(plt1, ax=axes)
 fig.tight_layout()
 
 plt.show()
+# %%
